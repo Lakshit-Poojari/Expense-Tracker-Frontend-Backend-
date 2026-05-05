7d0a23c7290bd8463f71ab59cecaca905362fd83
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-05-05 12:45:58 +0530

    create layout

--- frontend/src/Component/Dashboard/Layout/Layout.jsx
+++ frontend/src/Component/Dashboard/Layout/Layout.jsx
@@ -1,58 +1,116 @@
 import React, { useState } from 'react'
 import Table from '../Table/Table'
+import axios from "axios"
 
 function Layout() {
-  const [expense, setexpense] = useState({
+  const [expense, setExpense] = useState({
+    amount: "",
+    type: "",
+    category: "",
+    description: "",
+    date: ""
+  });
+  const [message, setmessage] = useState("")
 
-  })
+  const API = "http://localhost:3000/api/"
+
+  const handleChange = (e) => {
+    setExpense({
+      ...expense,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem("token");
+
+      const res = await axios.post(API + "createTransaction", expense,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+
+      setmessages(res.data.message);
+
+      // reset form
+      setExpense({
+        amount: "",
+        type: "",
+        category: "",
+        description: "",
+        date: ""
+      });
+
+    } catch (error) {
+      setmessages(error.message)
+    }
+  };
 
   return (
     <>
-        <div>
-          <h3>Expense Tracker</h3>
-        </div>
+      <div>
+        <h3>Expense Tracker</h3>
+      </div>
 
-        <div className='expense-input'>
-          <form >
+      <div className='expense-input'>
+        <form onSubmit={handleSubmit}>
 
-            <input
-              type="number"
-              name="amount"
-              placeholder="Enter amount"
-              
-              
-            />
+          <input
+            type="number"
+            name="amount"
+            placeholder="Enter amount"
+            value={expense.amount}
+            onChange={handleChange}
+            className='form-control'
+          />
 
-            <select
-              name="type"
-              
-            >
-              <option value="">Select Type</option>
-              <option value="income">Income</option>
-              <option value="expense">Expense</option>
-            </select>
+          <select
+            name="type"
+            value={expense.type}
+            onChange={handleChange}
+            className='form-control'
+          >
+            <option value="">Select Type</option>
+            <option value="income">Income</option>
+            <option value="expense">Expense</option>
+          </select>
 
-            <input
-              type="text"
-              name="category"
-              placeholder="Category (Food, Travel...)"
-              
-            />
+          <input
+            type="text"
+            name="category"
+            placeholder="Category"
+            value={expense.category}
+            onChange={handleChange}
+            className='form-control'
+          />
 
-            <input
-              type="text"
-              name="description"
-              placeholder="Description"
-              
-            />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={expense.description}
+            onChange={handleChange}
+            className='form-control'
+          />
 
-            <input type="date" />
+          <input
+            type="date"
+            name="date"
+            value={expense.date}
+            onChange={handleChange}
+            className='form-control'
+          />
 
-            <button type="submit">Add</button>
+          <button className='form-control' type="submit">Add</button>
 
-          </form>
-        </div>
-        <Table/>
+        </form>
+      </div>
+
+      <Table />
     </>
   )
 }
