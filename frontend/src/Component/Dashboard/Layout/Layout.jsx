5883ae0538c81e003aef64faaa1fa366cbdabebe
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-05-05 12:22:37 +0530

    creae Table

--- frontend/src/Component/Dashboard/Layout/Layout.jsx
+++ frontend/src/Component/Dashboard/Layout/Layout.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from '../Table/Table'
 
 function Layout() {
+  const [expense, setexpense] = useState({
+
+  })
+
   return (
     <>
-        <div>Layout</div>
+        <div>
+          <h3>Expense Tracker</h3>
+        </div>
+
+        <div className='expense-input'>
+          <form >
+
+            <input
+              type="number"
+              name="amount"
+              placeholder="Enter amount"
+              
+              
+            />
+
+            <select
+              name="type"
+              
+            >
+              <option value="">Select Type</option>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+
+            <input
+              type="text"
+              name="category"
+              placeholder="Category (Food, Travel...)"
+              
+            />
+
+            <input
+              type="text"
+              name="description"
+              placeholder="Description"
+              
+            />
+
+            <input type="date" />
+
+            <button type="submit">Add</button>
+
+          </form>
+        </div>
         <Table/>
     </>
   )
--- frontend/src/Component/Dashboard/Table/Table.jsx
+++ frontend/src/Component/Dashboard/Table/Table.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../Table/Table.css"
 
 function Table() {
+
+  const [expense, setexpense] = useState()
+  const [message, setmessage] = useState("")
   return (
-    <div>Table</div>
+    <>
+      <table>
+        <thead>
+          <tr className='table-heading'>
+            <td>Amount</td>
+            <td>Type</td>
+            <td>Category</td>
+            <td>Date</td>
+            <td>Description</td>
+          </tr>
+        </thead>
+        <tbody className='table-body'>
+            <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>0</td>
+            <td>9</td>
+            <td>8</td>
+            <td>7</td>
+            <td>6</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
 
